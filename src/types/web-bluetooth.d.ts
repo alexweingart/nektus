aaf48cde9b7661c35e4639b26f0d1b934d8f1d95
Alex Weingart <ajweingart@gmail.com>
2025-05-22 19:58:27 -0700

    Add Web Bluetooth TypeScript definitions to fix build error

---
+++ src/types/web-bluetooth.d.ts
@@ -0,0 +1,98 @@
+// Type definitions for Web Bluetooth API
+// These are custom type definitions to support the Web Bluetooth API which TypeScript doesn't include by default
+
+interface BluetoothRequestDeviceFilter {
+  services?: BluetoothServiceUUID[];
+  name?: string;
+  namePrefix?: string;
+  manufacturerId?: number;
+  serviceDataUUID?: BluetoothServiceUUID;
+}
+
+interface RequestDeviceOptions {
+  filters?: BluetoothRequestDeviceFilter[];
+  optionalServices?: BluetoothServiceUUID[];
+  acceptAllDevices?: boolean;
+}
+
+interface BluetoothRemoteGATTDescriptor {
+  characteristic: BluetoothRemoteGATTCharacteristic;
+  uuid: string;
+  value?: DataView;
+  readValue(): Promise<DataView>;
+  writeValue(value: BufferSource): Promise<void>;
+}
+
+interface BluetoothRemoteGATTCharacteristic {
+  service: BluetoothRemoteGATTService;
+  uuid: string;
+  properties: {
+    broadcast: boolean;
+    read: boolean;
+    writeWithoutResponse: boolean;
+    write: boolean;
+    notify: boolean;
+    indicate: boolean;
+    authenticatedSignedWrites: boolean;
+    reliableWrite: boolean;
+    writableAuxiliaries: boolean;
+  };
+  value?: DataView;
+  oncharacteristicvaluechanged: (event: Event) => void;
+  getDescriptor(descriptor: BluetoothDescriptorUUID): Promise<BluetoothRemoteGATTDescriptor>;
+  getDescriptors(descriptor?: BluetoothDescriptorUUID): Promise<BluetoothRemoteGATTDescriptor[]>;
+  readValue(): Promise<DataView>;
+  writeValue(value: BufferSource): Promise<void>;
+  writeValueWithResponse(value: BufferSource): Promise<void>;
+  writeValueWithoutResponse(value: BufferSource): Promise<void>;
+  startNotifications(): Promise<BluetoothRemoteGATTCharacteristic>;
+  stopNotifications(): Promise<BluetoothRemoteGATTCharacteristic>;
+  addEventListener(type: 'characteristicvaluechanged', listener: (event: Event) => void): void;
+  removeEventListener(type: 'characteristicvaluechanged', listener: (event: Event) => void): void;
+}
+
+interface BluetoothRemoteGATTService {
+  device: BluetoothDevice;
+  uuid: string;
+  isPrimary: boolean;
+  getCharacteristic(characteristic: BluetoothCharacteristicUUID): Promise<BluetoothRemoteGATTCharacteristic>;
+  getCharacteristics(characteristic?: BluetoothCharacteristicUUID): Promise<BluetoothRemoteGATTCharacteristic[]>;
+  getIncludedService(service: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService>;
+  getIncludedServices(service?: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService[]>;
+}
+
+interface BluetoothRemoteGATTServer {
+  device: BluetoothDevice;
+  connected: boolean;
+  connect(): Promise<BluetoothRemoteGATTServer>;
+  disconnect(): void;
+  getPrimaryService(service: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService>;
+  getPrimaryServices(service?: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService[]>;
+}
+
+interface BluetoothDevice extends EventTarget {
+  id: string;
+  name?: string;
+  gatt?: BluetoothRemoteGATTServer;
+  forget(): Promise<void>;
+  watchAdvertisements(): Promise<void>;
+  unwatchAdvertisements(): void;
+  addEventListener(type: string, listener: EventListener): void;
+  removeEventListener(type: string, listener: EventListener): void;
+}
+
+type BluetoothServiceUUID = string | number;
+type BluetoothCharacteristicUUID = string | number;
+type BluetoothDescriptorUUID = string | number;
+
+interface Bluetooth extends EventTarget {
+  getAvailability(): Promise<boolean>;
+  requestDevice(options?: RequestDeviceOptions): Promise<BluetoothDevice>;
+  getDevices(): Promise<BluetoothDevice[]>;
+  addEventListener(type: string, listener: EventListener): void;
+  removeEventListener(type: string, listener: EventListener): void;
+}
+
+interface Navigator {
+  bluetooth: Bluetooth;
+}
