48b4c878240f04df3715c0ab7df5500b60d6abc3
Alex Weingart <ajweingart@gmail.com>
2025-05-29 01:54:32 -0700

    Remove redundant utils.ts file in favor of app/utils/cn.ts implementation

--- src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-/**
- * A simple utility function to combine class names
- */
-export function cn(...classes: (string | undefined | null | false)[]) {
-  return classes.filter(Boolean).join(' ');
-}
