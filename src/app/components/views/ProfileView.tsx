48dbc6af49ad50856f722ccce43264b2f2604ed6
Alex Weingart <ajweingart@gmail.com>
2025-07-04 16:51:57 -0700

    Update service worker to include new asset revisions and routes

--- src/app/components/views/ProfileView.tsx
+++ src/app/components/views/ProfileView.tsx
@@ -232,7 +232,7 @@ const ProfileView: React.FC = () => {
               <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
             </svg>
             {hasUnconfirmedChannels && (
-              <div className="absolute -top-1 -right-1 w-4 h-4 bg-yellow-400 rounded-full border-2 border-white"></div>
+              <div className="absolute top-1 right-1 w-4 h-4 bg-yellow-400 rounded-full border-2 border-white"></div>
             )}
           </Button>
         </Link>
